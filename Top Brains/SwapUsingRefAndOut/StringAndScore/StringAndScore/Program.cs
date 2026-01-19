90741de64f5fb68fe1a2bee98283162c0ee6a1e3
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-19 12:30:16 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/StringAndScore/StringAndScore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+class Program
+{
+    static List<Student> StudentList = new List<Student>();
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Marks { get; set; }
+    }
+
+    public static void AddStudent(string input)
+    {
+        Student std = new Student();
+        string[] stdarray = input.Split(':');
+        std.Name = stdarray[0];
+        std.Marks = Int32.Parse(stdarray[1]);
+        StudentList.Add(std);
+    }
+    public static void Main(string[] args)
+    {
+ 
+        Console.WriteLine("Enter the number of Students you want to add");
+        int n = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Enter a string in format {Name:Score}");
+            string input = Console.ReadLine();
+            AddStudent(input);
+        }
+
+        var result = from m in StudentList
+                     orderby m.Marks descending , m.Name 
+                     select m;
+
+        foreach(Student s in result)
+        {
+            Console.WriteLine($"{s.Name} , {s.Marks}");
+        }
+
+        Console.WriteLine(JsonSerializer.Serialize(result));
+    }
+}

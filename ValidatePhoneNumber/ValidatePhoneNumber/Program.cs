5b864a36368593574e6dad4e41a69e72330c2606
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-19 09:38:37 +0530

    Add files via upload

---
+++ ValidatePhoneNumber/ValidatePhoneNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class Program
+{
+    public class User
+    {
+        public string Name {  get; set; }
+        public string PhoneNumber { get;
+            set
+            {
+                    if (value.Length == 10)
+                    {
+                        field = value;
+                    }
+                    else
+                    {
+                        throw new InvalidPhoneNumberExcetion("The Phone number you entered is Invalid");
+                    }
+                
+            }
+        }
+    }
+
+    internal class InvalidPhoneNumberExcetion : Exception
+    {
+        public InvalidPhoneNumberExcetion()
+        {
+
+        }
+
+        public InvalidPhoneNumberExcetion(string message) : base(message)
+        {
+
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        try { 
+        Console.WriteLine("Create a new User");
+        User newuser = new User();
+        Console.WriteLine("Enter User Name");
+        newuser.Name = Console.ReadLine();
+        Console.WriteLine("Enter User Phone Number");
+        newuser.PhoneNumber = Console.ReadLine();
+        }
+        catch (InvalidPhoneNumberExcetion e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+}

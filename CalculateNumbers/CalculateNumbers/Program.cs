e347e3b528b5eaa7bcadf88f7e0a881943edfcef
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-19 09:40:35 +0530

    Add files via upload

---
+++ CalculateNumbers/CalculateNumbers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+
+class Program
+{
+    public class EcommerceShop
+    {
+        public string UserName { get; set; }
+        public double WalletBalance { get; set; }
+        public double TotalPurchaseAmount { get; set; }
+
+        public void MakePayment(string name, double balance,double amount)
+        {
+            if(balance >= amount)
+            {
+                Console.WriteLine("Transaction Successful !!");
+            }
+            else
+            {
+                throw new InsufficientWalletBalanceException("Insufficient Balance in your digital Wallet");
+            }
+        }
+    }
+
+    internal class InsufficientWalletBalanceException : Exception
+    {
+        public InsufficientWalletBalanceException()
+        {
+
+        }
+        public InsufficientWalletBalanceException(string message) : base(message)
+        {
+
+        }
+    }
+    public static void Main(string[] args)
+    {
+        try
+        {
+            EcommerceShop newShopee = new EcommerceShop();
+            Console.WriteLine("Enter UserName");
+            newShopee.UserName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Wallet Balance");
+            newShopee.WalletBalance = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Total Purchase Amount");
+            newShopee.TotalPurchaseAmount = Double.Parse(Console.ReadLine());
+            newShopee.MakePayment(newShopee.UserName, newShopee.WalletBalance, newShopee.TotalPurchaseAmount);
+            
+        }
+
+        catch(InsufficientWalletBalanceException e)
+        {
+
+            Console.WriteLine($"Error: {e.Message}");
+        }
+
+        catch (Exception e)
+        {
+
+            Console.WriteLine($"Error: {e.Message}");
+        }
+
+
+
+
+    }
+}

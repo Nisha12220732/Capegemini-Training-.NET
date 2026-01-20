eebb5c3387dd53c16ee8693bae7dbf5467be9a50
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-20 15:05:28 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Time in seconds");
+        int time = Int32.Parse(Console.ReadLine());
+        int sec = time % 60;
+        int min = time / 60;
+        if(sec < 10)
+        {
+            Console.WriteLine($"Time is {min}:0{sec}");
+        }
+        else
+        {
+            Console.WriteLine($"Time is {min}:{sec}");
+        }
+    }
+}

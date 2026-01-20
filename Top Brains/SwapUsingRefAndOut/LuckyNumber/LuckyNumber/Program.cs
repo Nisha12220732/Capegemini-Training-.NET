6d38acf6366c0454ab8e6b24c5d52be7f1244745
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-21 00:13:44 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/LuckyNumber/LuckyNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    public static int sumOfDigits(int n)
+    {
+        int sum = 0;
+        while(n/10 != 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum + n;
+    }
+    public static bool luckyNumber(int num)
+    {
+        if(sumOfDigits(num) == (int)Math.Sqrt(sumOfDigits(num * num)))
+        {
+            return true; 
+        }
+        return false;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the range ");
+        Console.Write("Starting from  ");
+        int a = Int32.Parse(Console.ReadLine());
+        Console.Write("to ");
+        int b = Int32.Parse(Console.ReadLine());
+
+        int count = 0;
+        for (int i = a; i <= b; i++)
+        {
+            
+            if (luckyNumber(i))
+            {
+                Console.WriteLine($"{i} is a Lucky Number");
+                count++;
+            }
+        }
+        Console.WriteLine($"Total lucky numbers between {a} and {b} are {count}");
+    }
+}

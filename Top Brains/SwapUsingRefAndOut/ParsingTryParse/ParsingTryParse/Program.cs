790ae8670970bbdb7cca8f9c2d7cd82d3b9aa6d6
Nisha12220732 <119564022+Nisha12220732@users.noreply.github.com>
2026-01-21 15:47:16 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/ParsingTryParse/ParsingTryParse/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the size of you array");
+        int n = Int32.Parse(Console.ReadLine());
+        string[] arr = new string[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = Console.ReadLine();
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if (int.TryParse(arr[i],out int v)){
+                sum += v;
+
+            }
+        }
+
+        Console.WriteLine(sum);
+
+    }
+}

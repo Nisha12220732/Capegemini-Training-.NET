60dd1e8660e9c3a700c2751bfa597c1ba3c14150
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-01-25 20:20:25 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/CustomSorting/CustomSorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using CustomSorting;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Student> students = null;
+        students = new List<Student>()
+        {
+            new Student{Name = "Nisha" , Age = 23 , Marks = 45},
+            new Student{Name = "Nish" , Age = 21 , Marks = 40},
+            new Student{Name = "Neha" , Age = 20 , Marks = 47},
+            new Student{Name = "Nia" , Age = 29 , Marks = 47},
+            new Student{Name = "Ni" , Age = 26, Marks = 40},
+            new Student{Name = "isha" , Age = 24 , Marks = 45},
+            new Student{Name = "Trisha" , Age = 19 , Marks = 50},
+            
+        };
+
+        var anslist = from e in students
+                      orderby e.Marks descending, e.Age 
+                      select e;
+
+        Console.WriteLine("Your Answer List is: ");
+
+        foreach(Student student in anslist)
+        {
+            Console.WriteLine($"Name: {student.Name} | Age: {student.Age} | Marks: {student.Marks}");
+        }
+
+
+    }
+}
---
+++ Top Brains/SwapUsingRefAndOut/CustomSorting/CustomSorting/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomSorting
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age {  get; set; }
+        public int Marks { get; set; }
+
+    }
+}

cb9837bf9fa781ad8257a29612c02c1afc2aad80
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-10 01:39:11 +0530

    Add files via upload

---
+++ Top Brains/SwapUsingRefAndOut/ProductDiscountEligible-PredicateDelegate/ProductDiscountEligible-PredicateDelegate/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsPremiumCustomer { get; set; }
+    }
+
+    public class DiscountEngine
+    {
+        public void CheckDiscount(Product product, string scheme, Predicate<Product> rule)
+        {
+            bool isEligible = rule(product);
+            Console.WriteLine("========= DISCOUNT ELIGIBILITY =========");
+            Console.WriteLine($"Product  : {product.Name}");
+            Console.WriteLine($"Scheme   : {scheme}");
+            Console.WriteLine($"Eligible : {isEligible}");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine();
+
+        }
+    }
+
+    public static void Main()
+    {
+        // STEP 1: Create product object (hardcoded dataset)
+        Product product = new Product
+        {
+            ProductId = 901,
+            Name = "Laptop",
+            Price = 60000,
+            Quantity = 12,
+            IsPremiumCustomer = true
+        };
+
+        Predicate<Product> festivalDiscountRule = a => a.Price >= 5000;
+        Predicate<Product> BulkDiscountRule = a => a.Quantity >= 10;
+        Predicate<Product> premiumDiscountRule = a => a.IsPremiumCustomer == true;
+
+        DiscountEngine dobj = new DiscountEngine();
+        dobj.CheckDiscount(product, "Festival", festivalDiscountRule);
+        dobj.CheckDiscount(product, "Bulk", BulkDiscountRule);
+        dobj.CheckDiscount(product, "Premium", premiumDiscountRule);
+
+
+
+    }
+}

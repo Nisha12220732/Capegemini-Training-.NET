21c46630619b6d2844118b9d0bfcabe3c4ee8641
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-11 23:24:04 +0530

    Add files via upload

---
+++ PracticeQuestionMockTest/PracticeQuestionMockTest/Program.cs
@@ -0,0 +1,69 @@
+﻿class Program
+{
+    public static bool IsVowel(char ch)
+    {
+        if(ch=='a'|| ch == 'e' || ch == 'i' || ch == 'o'|| ch == 'u')
+        {
+            return true;
+        }
+        return false;
+    }
+    public static char NextVowel(char ch)
+    {
+        if (ch == 'a')
+        {
+            return 'e';
+        }
+        if (ch == 'e')
+        {
+            return 'i';
+        }
+        if (ch == 'i')
+        {
+            return 'o';
+        }
+        if (ch == 'o')
+        {
+            return 'u';
+        }
+        if (ch == 'u')
+        {
+            return 'a';
+        }
+        return ch;
+
+    }
+
+
+    public static void Main(string[] args)
+    {
+        string str = "hello";
+        int rotate = 2;
+        String str8 = new string(str.Reverse().ToArray());
+        String newstr = new String(str8.Distinct().ToArray());
+        Console.WriteLine(newstr);
+        string str2 = "";
+        for(int i = 0; i < newstr.Length; i++)
+        {
+            if (IsVowel(newstr[i]))
+            {
+                str2 += NextVowel(newstr[i]);
+            }
+            else
+            {
+                str2 += newstr[i];
+            }
+
+        }
+
+
+        string str3 = str2.Substring(0, rotate);
+        Console.WriteLine(str3);
+        string str4 = str2.Substring(rotate);
+        Console.WriteLine(str4);
+        string str5 = str4+ str3;
+        Console.WriteLine(str5);
+
+        Console.WriteLine("abc" == "abc");
+    }
+}

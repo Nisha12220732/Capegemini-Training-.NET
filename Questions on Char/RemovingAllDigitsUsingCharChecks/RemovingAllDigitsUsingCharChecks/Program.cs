7ef7788b53be817f560150b4ca83337289e7b8b8
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-13 19:49:27 +0530

    Add files via upload

---
+++ Questions on Char/RemovingAllDigitsUsingCharChecks/RemovingAllDigitsUsingCharChecks/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your String");
+        string str = Console.ReadLine();
+        string newstr = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            char ch = str[i];
+            if (!char.IsDigit(ch))
+            {
+                newstr += ch;
+            }
+        }
+        Console.WriteLine(newstr);
+    }
+}

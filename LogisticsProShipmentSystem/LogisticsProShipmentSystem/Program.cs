f9a281812907f04def92fc0206d496f5365c4b22
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-14 20:57:43 +0530

    Add files via upload

---
+++ LogisticsProShipmentSystem/LogisticsProShipmentSystem/Program.cs
@@ -0,0 +1,36 @@
+﻿using LogisticsProShipmentSystem;
+using System;
+class Program
+{
+    public static void Main(string [] args)
+    {
+        Console.WriteLine("Enter Shipment Code: ");
+        string shipmentcode = Console.ReadLine();
+
+        ShipmentDetails sObj = new ShipmentDetails();
+        //ShipmentDetails sObj = new ShipmentDetails();
+
+
+        if (!sObj.ValidateShipmentCode(shipmentcode))
+        {
+            Console.WriteLine("Invalid Shipment Code");
+        }
+        else
+        {
+            Console.WriteLine("Enter Transport Mode: ");
+            string mode = Console.ReadLine();
+            Console.WriteLine("Enter Weight of Shipment");
+            double weight = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("No. of Storage Days");
+            int days = Int32.Parse(Console.ReadLine());
+
+            sObj.ShipmentCode = shipmentcode;
+            sObj.TransportMode = mode;   
+            sObj.Weight = weight;
+            sObj.StorageDays = days;
+
+            Console.WriteLine($"The total shipping cost is {sObj.CalculateTotalCost()}");
+        }
+
+    }
+}
---
+++ LogisticsProShipmentSystem/LogisticsProShipmentSystem/ShipmentDetails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LogisticsProShipmentSystem
+{
+    public class ShipmentDetails : Shipment
+    {
+        public bool ValidateShipmentCode(string str)
+        {
+            if (Regex.IsMatch(str, @"^[G]{1}[C]{1}[#]{1}[0-9]{4}"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public double CalculateTotalCost()
+        {
+            double RatePerKg = 0;
+            if (TransportMode.Equals("Sea"))
+            {
+                RatePerKg = 15.00;
+            }
+            else if (TransportMode.Equals("Air"))
+            {
+                RatePerKg = 50.00;
+            }
+            else if (TransportMode.Equals("Land"))
+            {
+                RatePerKg = 25.00;
+            }
+            else
+            {
+                throw new InvalidTransportModeException("Transport Mode is Invalid!!");
+            }
+            return Math.Round((Weight * RatePerKg) + Math.Sqrt(StorageDays),2);
+
+        }
+
+        
+
+    }
+    public class InvalidTransportModeException : Exception
+    {
+        public InvalidTransportModeException() : base()
+        {
+
+        }
+        public InvalidTransportModeException(string str): base(str)
+        {
+
+        }
+    }
+
+}

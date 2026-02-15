4de1033372b2c63ddd470630fed22ae51065eb55
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-15 23:40:26 +0530

    Add files via upload

---
+++ Questions on Char/RemovingAllDigitsUsingCharChecks/CheckToggleCase/CheckToggleCase/Program.cs
@@ -0,0 +1,62 @@
+﻿class Program
+{
+    public static bool IsToggle(string str)
+    {
+        char ch = str[0];
+        string newstr = str[0].ToString();
+        if (char.IsLower(ch))
+        {
+            for (int i = 1; i < str.Length; i++)
+            {
+                char ch1 = str[i];
+                if (i % 2 == 0)
+                {
+                    if (!char.IsLower(ch1))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (!char.IsUpper(ch1))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        else
+        {
+            for (int i = 1; i < str.Length; i++)
+            {
+                char ch1 = str[i];
+                if (i % 2 == 0)
+                {
+                    if (!char.IsUpper(ch1))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if(!char.IsLower(ch1))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your String: ");
+        string str = Console.ReadLine();
+
+        Console.WriteLine($"Given String is in Toggle Case: {IsToggle(str)}");
+
+    }
+}

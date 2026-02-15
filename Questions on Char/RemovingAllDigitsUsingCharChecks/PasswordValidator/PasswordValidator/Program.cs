5fbcc61fadef3a37b271f5750696d0f280b9e568
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-15 23:41:38 +0530

    Add files via upload

---
+++ Questions on Char/RemovingAllDigitsUsingCharChecks/PasswordValidator/PasswordValidator/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your Password: ");
+        string str = Console.ReadLine();
+        int uppercase = 0;
+        int lowercase = 0;
+        int specialCharacter = 0;
+        int digitcount = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsUpper(str[i]))
+            {
+                uppercase++;
+            }
+            if (char.IsLower(str[i]))
+            {
+                lowercase++;
+            }
+           
+            if (char.IsSymbol(str[i]) || char.IsPunctuation(str[i]))
+            {
+                specialCharacter++;
+            }
+            if (char.IsDigit(str[i]))
+            {
+                digitcount++;
+            }
+        }
+        //Console.WriteLine($"Number of Upper Case Character in your string is {uppercase}");
+        //Console.WriteLine($"Number of Lower Case Character in your string is {lowercase}");
+        //Console.WriteLine($"Number of  Special Character in your string is {specialCharacter}");
+        //Console.WriteLine($"Number of  Digit in your string is {digitcount}");
+
+        if(uppercase > 0 && lowercase > 0 && specialCharacter > 0 && digitcount > 0 && str.Length >= 8)
+        {
+            Console.WriteLine("Strong Password !!");
+        }
+        else
+        {
+            Console.WriteLine("Weak Password !!");
+        }
+
+
+    }
+}

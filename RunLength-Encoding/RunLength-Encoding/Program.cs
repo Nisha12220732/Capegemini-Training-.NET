11b2fa956380ae1c5ae6e243da2d18b98bc5608d
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-19 22:49:40 +0530

    Add files via upload

---
+++ RunLength-Encoding/RunLength-Encoding/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your string: ");
+        string str = Console.ReadLine();
+        char prevChar = str[0];
+        string ansstr = prevChar.ToString();
+        int count = 1;
+        for(int i = 1; i < str.Length; i++)
+        {
+            if (str[i] == prevChar)
+            {
+                count++;
+            }
+            else
+            {
+                ansstr += count;
+                count = 1;
+                ansstr += str[i];
+                prevChar = str[i];
+            }
+        }
+       
+              
+        ansstr += count;
+        Console.WriteLine(ansstr);
+    }
+}

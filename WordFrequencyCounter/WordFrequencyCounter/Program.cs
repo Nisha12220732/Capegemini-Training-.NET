9a9448482ff0aeeafe3f3901171b64acf557db9f
Nisha Agarwal <119564022+Nisha12220732@users.noreply.github.com>
2026-02-21 23:17:06 +0530

    Add files via upload

---
+++ WordFrequencyCounter/WordFrequencyCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your String: ");
+        string str = (Console.ReadLine()).ToLower();
+        string[] str1 = str.Split(" ");
+        SortedDictionary<String, int> freq = new SortedDictionary<string, int>();
+        for(int i =0;i< str1.Length; i++)
+        {
+            if (freq.ContainsKey(str1[i]))
+            {
+                freq[str1[i]]++;
+            }
+            else
+            {
+                freq.Add(str1[i], 1);
+            }
+        }
+        foreach(var i in freq)
+        {
+            Console.WriteLine($"{i.Key}: {i.Value}");
+        }
+    }
+}
